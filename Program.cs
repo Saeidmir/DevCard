c12150cfd930e0c336de13ba23a67476179cbf1b
Saeid mirakhorlo <saeid.mirakhorlo@gmail.com>
2022-11-30 14:52:31 +0330

    index and contact view pages added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.MapDefaultControllerRoute();
+app.UseStaticFiles();
 app.Run();
